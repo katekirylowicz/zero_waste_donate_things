01ae21d8a4142e166b47420ed25c08c2dc3c61bb
Katarzyna Kiryłowicz <k.kirylowicz@gmail.com>
2022-01-12 11:34:15 +0100

    Initial creat react app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+
+        </p>
+        <a
+
+        >
+          start zerow_weste_donate
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
