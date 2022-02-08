a084c45f6853d90926fe4c1114bcdbb1caf30211
Katarzyna Kiryłowicz <k.kirylowicz@gmail.com>
2022-02-08 23:40:13 +0100

    Updates

---
+++ precache-manifest.475b88af8b29d485be839156dc8c616e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153f3c436573e70b50a5073d372383a2",
+    "url": "/zero_waste_donate_things/index.html"
+  },
+  {
+    "revision": "07f5bd47c4e0996128d5",
+    "url": "/zero_waste_donate_things/static/css/main.69cc6e09.chunk.css"
+  },
+  {
+    "revision": "0eac68a34f395721f0e5",
+    "url": "/zero_waste_donate_things/static/js/2.28d5bca2.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/zero_waste_donate_things/static/js/2.28d5bca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f5bd47c4e0996128d5",
+    "url": "/zero_waste_donate_things/static/js/main.7e057ea5.chunk.js"
+  },
+  {
+    "revision": "b66e0edd80031a5ade9a",
+    "url": "/zero_waste_donate_things/static/js/runtime-main.6a19ac7e.js"
+  }
+]);
